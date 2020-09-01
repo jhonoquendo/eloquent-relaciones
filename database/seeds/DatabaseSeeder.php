02105e0ddd32047444035fd7aada0b11e9bb5f96
jhonoquendo <32801020+jhonoquendo@users.noreply.github.com>
2020-08-31 19:13:50 -0500

    19 Se configura DatabaseSeeder para ejecutar la creacion de datos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,21 @@ class DatabaseSeeder extends Seeder
         factory(App\Level::class)->create(['name' => 'Plata']);
         factory(App\Level::class)->create(['name' => 'Bronce']);
 
-        
+        factory(App\User::class,5)->create()->each(function ($user){
+            $profile = $user->profile()->save(factory(App\Profile::class)->make());
+            $profile->location()->save(factory(App\Location::class)->make());
+            $user->groups()->attach($this->array(rand(1,3)));
+            $user->image()->save(factory(App\Image::class)->make([
+                'url'=>'https://lorempixel.com/90/90'
+            ]));
+        });
+    }
+
+    public function array($max){
+        $values = [];
+        for($i=1;$i<$max;$i++){
+            $values[] = $i;
+        }
+        return $values;
     }
 }
